416acf0dff20ec7dd5c5e00b8c78705db2be0ebd
dev_eloper.ai <devendrakarma1803@gmail.com>
2025-03-01 15:23:32 +0530

    homepage added sections

--- include/constant.php
+++ include/constant.php
@@ -1,10 +1,10 @@
 <?php
 if (!defined('BASE_URL')) {
-   define('BASE_URL', 'http://localhost/aira-company-project');	
-   // define('BASE_URL', 'https://web.aira.technology/static-html/');	
+   // define('BASE_URL', 'http://localhost/aira-company-project');	
+   define('BASE_URL', 'https://web.aira.technology/static-html/');	
 }
 if (!defined('IMAGES_URL')) {
-   define('IMAGES_URL', 'http://localhost/aira-company-project/assets/images');
-   // define('IMAGES_URL', 'https://web.aira.technology/static-html/assets/images');
+   // define('IMAGES_URL', 'http://localhost/aira-company-project/assets/images');
+   define('IMAGES_URL', 'https://web.aira.technology/static-html/assets/images');
 }
 ?>
